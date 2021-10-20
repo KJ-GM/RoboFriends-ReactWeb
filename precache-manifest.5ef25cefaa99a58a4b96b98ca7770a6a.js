7ec6e5c297f6cb7c19c5940e32b3b7e6e2428036
Karam Elgamal <Karamjammal6@gmail.com>
2021-10-20 15:05:17 +0300

    Updates

---
+++ precache-manifest.5ef25cefaa99a58a4b96b98ca7770a6a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33e1da799fcf4f01eabc035906bb95a",
+    "url": "/RoboFriends-ReactWeb/index.html"
+  },
+  {
+    "revision": "ea3a900cf285cb10515e",
+    "url": "/RoboFriends-ReactWeb/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "6dc41e0fc45a5690521d",
+    "url": "/RoboFriends-ReactWeb/static/css/main.c1faf7c8.chunk.css"
+  },
+  {
+    "revision": "ea3a900cf285cb10515e",
+    "url": "/RoboFriends-ReactWeb/static/js/2.639989a7.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/RoboFriends-ReactWeb/static/js/2.639989a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f3d753461fa6c0bc0a",
+    "url": "/RoboFriends-ReactWeb/static/js/3.32e500d0.chunk.js"
+  },
+  {
+    "revision": "6dc41e0fc45a5690521d",
+    "url": "/RoboFriends-ReactWeb/static/js/main.63578c2b.chunk.js"
+  },
+  {
+    "revision": "b058d04ea150ba80140c",
+    "url": "/RoboFriends-ReactWeb/static/js/runtime-main.03648b72.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends-ReactWeb/static/media/SEGA.12a3db8b.woff"
+  }
+]);
